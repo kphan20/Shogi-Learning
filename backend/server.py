5a47e026efc877c69fe92d1431e7c789c8c5c454
kphan20 <81403321+kphan20@users.noreply.github.com>
2022-05-14 18:06:51 -0400

    feat: started socket communication and piece movement

--- backend/server.py
+++ backend/server.py
@@ -1,17 +1,43 @@
 from flask import Flask
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
+from shogi_logic import find_moves_for_piece, move_to_board, rotate_board, check_check
+import numpy as np
 
 # add secret key later
 app = Flask(__name__)
 app.config["DEBUG"] = True
-socketio = SocketIO(app)
+# should change allowed origins later
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-@socketio.on("json")
-def handle_message(data):
-    print(data)
-    send(data)
+@socketio.on("connect")
+def test_connect():
+    emit("after connect", "Connection success")
+
+
+# given a piece and the board, calculate possible moves in the position
+@socketio.on("move")
+def send_moves(data):
+    board = np.array(data["board"])
+    player = data["color"]
+    moves = find_moves_for_piece(board, player, data["rank"], data["file"])
+
+    # filter illegal moves
+    moves = [
+        move
+        for move in moves
+        if not check_check(rotate_board(move_to_board(board, player, move)), player)
+    ]
+
+    # translates Move object into array
+    moves = [[list(move.piece), list(move.dest), move.promote] for move in moves]
+    emit("possible moves", moves)
+
+
+@socketio.on("disconnect")
+def client_disconnect():
+    print("Connection closed")
 
 
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app, host="0.0.0.0")
